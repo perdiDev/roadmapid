bf4efb02bc1cc7a889bc60ac283eb34d4ca1908d
PerdiDev <perdidev@gmail.com>
2023-01-21 15:26:14 -0800

    Memperbaiki header

--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -5,11 +5,11 @@ export default function Header() {
         <header className="container">
             <div className="relative">
                 <div className="absolute top-0 left-0 right-0">
-                    <Link className="flex w-full justify-center py-4" href="/">
-                        <div className="py-2 px-4 card-color rounded-lg">
+                    <div className="flex w-full justify-center py-4">
+                        <Link className="py-2 px-4 card-color rounded-lg" href="/">
                             <h1 className="text-white-1 font-bold text-lg">Roadmap<span className="text-red-600">Id</span></h1>
-                        </div>
-                    </Link>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </header>
