d71ecdd0f82ec1d345a4fef17550a2c872ec069f
PerdiDev <perdidev@gmail.com>
2023-01-23 22:44:09 -0800

    Mengupdate isi kontent dari dasar web, sumber belajar dan deskripsi

--- app/frontend/dasarweb/page.tsx
+++ app/frontend/dasarweb/page.tsx
@@ -1,12 +1,16 @@
 import ButtonBack from '@/app/components/buttonBack'
 import CardDesc from '@/app/components/cardDesc'
+import CardNote from '@/app/components/cardNote'
 import SourceBelajar from '@/app/components/sourceBelajar'
 import TitlePage from '@/app/components/titlePage'
 
 export default function Html() {
     const sources = [
-        {icon: 'link', detail: "HTTP-Dicoding Blog (Cukup dibaca dan tau aja, Tapi wajib)", link: "https://www.dicoding.com/blog/perbedaan-http-dan-https/"},
-        {icon: 'youtube', detail: "Belajar HTTP-PZN (Video panjang, optional aja!)", link: "https://www.youtube.com/watch?v=92Rjzrq4oIg"},
+        {icon: 'link', detail: "HTTP - Dicoding Blog", link: "https://www.dicoding.com/blog/perbedaan-http-dan-https/"},
+        {icon: 'link', detail: "Apa itu domain - Hostinger", link: "https://www.hostinger.co.id/tutorial/apa-itu-domain/"},
+        {icon: 'link', detail: "DNS - Hostinger", link: "https://www.niagahoster.co.id/blog/apa-itu-dns/"},
+        {icon: 'youtube', detail: "Belajar HTTP - Programer Zaman Now (Optional)", link: "https://www.youtube.com/watch?v=92Rjzrq4oIg"},
+        {icon: 'link', detail: "How does internet work - MDN (Lengkap - Berbahasa inggris)", link: "https://developer.mozilla.org/en-US/docs/Learn/Common_questions/How_does_the_Internet_work"},
     ]
     return (
         <section id="main" className="container">
@@ -17,8 +21,9 @@ export default function Html() {
                     backTo="Frontend" 
                 />
                 <CardDesc 
-                    desc="Belajar tentang cara kerja website melalui jaringan internet, mengerti bagaimana browser beroperasi, serta memahami cara kerja DNS dan domain." 
+                    desc="Mempelajari bagaimana internet bisa berkomunikasi dengan komputer yang ada didunia untuk menyediakan beragam informasi. Komunikasi tersebut memiliki standarisasi yang berlaku secara global. Dasar tersebut akan memberikan kemampuan pada browser untuk menampilkan informasi yang dibutuhkan oleh pengguna. Tambahannya, kita perlu mengetahui bagaimana informasi bisa diakses secara online melalui penamaan domain. Seperti google.com, youtube.com, github.io, dll." 
                 />
+                <CardNote desc="Cukup pahami bagaimana internet bekerja. Ini hanyalah pengetahuan dasar yang dibutuhkan untuk memulai perjalanan belajar frontend." />
                 <SourceBelajar resources={sources} />
                 <ButtonBack linkBack="/frontend" />
             </div>
