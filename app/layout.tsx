46ac8339bf38b9f976cafa9c5983078746249cd8
PerdiDev <perdidev@gmail.com>
2023-01-24 16:20:40 -0800

    Menambahkan vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Footer from './layout/footer'
 import './globals.css'
 import Header from './layout/header'
 import { Jaldi } from '@next/font/google'
+import { AnalyticsWrapper } from './components/analytics';
 
 const jaldi = Jaldi({
   weight: ["400", "700"],
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className='background'>
         <Header />
           {children}
+          <AnalyticsWrapper />
         <Footer />
       </body>
     </html>
