221e7e9ab344065a738ee1a6d3083c51c7e2f93d
PerdiDev <perdidev@gmail.com>
2023-02-21 07:46:00 +0800

    Menambahkan lib alurBelajar

--- app/frontend/page.tsx
+++ app/frontend/page.tsx
@@ -1,42 +1,41 @@
-import CardList from "@/app/components/cardList"
-import TitlePage from "@/app/components/titlePage"
-import CardDesc from "@/app/components/cardDesc"
-import CardNote from "@/app/components/cardNote"
+import CardList from "@/app/components/cardList";
+import TitlePage from "@/app/components/titlePage";
+import CardDesc from "@/app/components/cardDesc";
+import CardNote from "@/app/components/cardNote";
 
 export default function Frontend() {
-    const listAlur = [
-        {linkTo: "dasarweb", jalur: "Dasar Web"},
-        {linkTo: "html", jalur: "HTML"},
-        {linkTo: "css", jalur: "CSS"},
-        {linkTo: "javascript", jalur: "Javascript"},
-        {linkTo: "bootstrap", jalur: "Bootstrap"},
-        {linkTo: "git", jalur: "Git"},
-        {linkTo: "npm", jalur: "NPM"},
-        {linkTo: "react", jalur: "React"},
-        {linkTo: "tailwindcss", jalur: "Tailwindcss"},
-        {linkTo: "nextjs", jalur: "Next Js"},
-        {linkTo: "api", jalur: "API"},
-        {linkTo: "projek", jalur: "Proyek Akhir"},
-    ];
-    let num = 0;
-    return (
-        <section id="main" className="container">
-            <div className="hero-section space-y-8">
-                <TitlePage 
-                    title="Frontend" 
-                    linkBack="/" 
-                    backTo="Home" 
-                />
-                <CardDesc 
-                    desc="Frontend adalah bagian yang berinteraksi dengan pengguna. Dalam hal ini semua yang tampil di browser adalah tugas serorang frontend developer untuk mengatur tampilan dan interaksinya dengan pengguna. Selain itu, frontend dev bertugas untuk memanfaatkan API yang dibuat oleh backend agar bisa dimanfaatkan pada browser untuk mebuat website lebih interaktif dan dinamis." 
-                />
-                <CardNote desc="Alur belajar yang disediakan berdasarkan pengalaman dan riset. Oleh karena itu, alur belajar bisa disesuaikan dengan kebutuhan masing-masing." />
-                <div className="layout-grid">
-                    {listAlur.map(list =>
-                        <CardList listNumber={num++} linkTo={`/frontend/${list.linkTo}`} jalur={list.jalur}  key={num.toString()} />
-                    )}
-                </div>
-            </div>
-        </section>
-    )
+  const listAlur = [
+    { linkTo: "dasarweb", jalur: "Dasar Web" },
+    { linkTo: "html", jalur: "HTML" },
+    { linkTo: "css", jalur: "CSS" },
+    { linkTo: "javascript", jalur: "Javascript" },
+    { linkTo: "bootstrap", jalur: "Bootstrap" },
+    { linkTo: "git", jalur: "Git" },
+    { linkTo: "npm", jalur: "NPM" },
+    { linkTo: "react", jalur: "React" },
+    { linkTo: "tailwindcss", jalur: "Tailwindcss" },
+    { linkTo: "nextjs", jalur: "Next Js" },
+    { linkTo: "api", jalur: "API" },
+    { linkTo: "projek", jalur: "Proyek Akhir" },
+  ];
+  let num = 0;
+  return (
+    <section id="main" className="container">
+      <div className="hero-section space-y-8">
+        <TitlePage title="Frontend" linkBack="/" backTo="Home" />
+        <CardDesc desc="Frontend adalah bagian yang berinteraksi dengan pengguna. Dalam hal ini semua yang tampil di browser adalah tugas serorang frontend developer untuk mengatur tampilan dan interaksinya dengan pengguna. Selain itu, frontend dev bertugas untuk memanfaatkan API yang dibuat oleh backend agar bisa dimanfaatkan pada browser untuk mebuat website lebih interaktif dan dinamis." />
+        <CardNote desc="Alur belajar yang disediakan berdasarkan pengalaman dan riset. Oleh karena itu, alur belajar bisa disesuaikan dengan kebutuhan masing-masing." />
+        <div className="layout-grid">
+          {listAlur.map((list) => (
+            <CardList
+              listNumber={num++}
+              linkTo={`/frontend/${list.linkTo}`}
+              jalur={list.jalur}
+              key={num.toString()}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,30 @@
-import Button from "./components/button"
+import Button from "./components/button";
+import alurBelajar from "@/lib/alurBelajar.json";
 
 export default function Home() {
   return (
     <section id="main" className="container">
-        <div className="hero-section">
-            <div className="flex flex-col">
-                <div className="w-full pb-6 md:py-12 text-center space-y-3 md:space-y-6 lg:space-y-7">
-                    <h1 className="heading-utama">Mulai perjalananmu dari sini!</h1>
-                    <h1 className="heading-kedua">Sebuah jalan untuk berkarya</h1>
-                </div>
-                <div className="w-full flex justify-center md:px-12">
-                    <div className="w-full card-color py-20 rounded-xl">
-                        <div className="w-full flex flex-col md:flex-row gap-8 justify-center px-6">
-                            <Button name="Frontend" direksi="frontend" />
-                            <Button name="Backend" direksi="backend" />
-                        </div>
-                    </div>
-                </div>
+      <div className="hero-section">
+        <div className="flex flex-col">
+          <div className="w-full pb-6 md:py-12 text-center space-y-3 md:space-y-6 lg:space-y-7">
+            <h1 className="heading-utama">Mulai perjalananmu dari sini!</h1>
+            <h1 className="heading-kedua">Sebuah jalan untuk berkarya</h1>
+          </div>
+          <div className="w-full flex justify-center md:px-12">
+            <div className="w-full card-color py-20 rounded-xl">
+              <div className="w-full flex flex-col md:flex-row gap-8 justify-center px-6">
+                {alurBelajar.map((alurDireksi, i) => (
+                  <Button
+                    key={i}
+                    name={alurDireksi.alur}
+                    direksi={alurDireksi.alur.toLowerCase()}
+                  />
+                ))}
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </section>
-  )
+  );
 }
