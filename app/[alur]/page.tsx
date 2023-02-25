65e2e34f26df3a4c74a8d2998ddd1e26f1d780bf
PerdiDev <perdidev@gmail.com>
2023-02-25 16:07:13 +0800

    Mengorganisir file lib dan data

--- app/[alur]/page.tsx
+++ app/[alur]/page.tsx
@@ -3,7 +3,7 @@ import TitlePage from "@/app/components/titlePage";
 import CardDesc from "@/app/components/cardDesc";
 import CardNote from "@/app/components/cardNote";
 
-import alurBelajar from "@/lib/alurBelajar.json";
+import alurBelajar from "@/data/alurBelajar.json";
 import slugify from "@/lib/slugify";
 
 export async function generateStaticParams() {
